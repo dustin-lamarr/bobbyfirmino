8d86ea1f48b24018408672f3dfbb74db20f44956
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-12-18 08:41:02 -0700

    added list of PL teams for links to pages

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,20 +1,10 @@
 const router = require('express').Router();
 var path = require("path");
 
-
-// router.get('/', (req, res) => {
-//     try {
-//         // res.render('index')
-//         res.sendFile(path.join(__dirname, "./views/index.html"));
-//         res.status(200).json();
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../views/index.html'));
   });
 
+  
 // router.use('/')
 module.exports = router;
