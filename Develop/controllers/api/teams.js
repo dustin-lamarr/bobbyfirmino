572e2e1d4767ab058c90899aea60c04be129a325
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-12-21 09:48:54 -0700

    fixed api get route for all teams

--- Develop/controllers/api/teams.js
+++ Develop/controllers/api/teams.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const Bobby = require('../../models');
 
-router.get('/liverpool', async (req,res) => {
-    res.redirect('/');
+router.get('/', async (req,res) => {
+    try {   
+        const gifData = await Bobby.findAll();
+        // res.send(gifData)
+    res.status(200).json(gifData);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
+
+// router.get('/liverpool', async (req,res) => {
+//     try{
+//         const gifData = await Bobby.findAll();
+//         res.status(200).json(gifData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+//     // res.redirect('/');
+
+// });
 
 // router.get('tottenham', async (req, res) => {
     
 // })
+
+module.exports = router;
