c96c99ad0ecfe89a5bf68dd101d3df0d580ac601
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-12-02 11:55:34 -0700

    I gotta commit more often
    removed files
    updated tailwind config
    fixed nav link position
    added kit custom styling svgs

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -5,13 +5,13 @@ const { Bobby, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('home');
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         res.render('home');
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      red: '#B91331',
+      green: '#1E4E56',
+      yellow: '#fef667',
+      orange: '#F7664D',
+      cream: '#ECE7E1',
+      black: 'black'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
